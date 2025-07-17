c96147250bc3a8403b147e90041c763c286283ad
vikashk.mn24 <vikash1998bscc@gmail.com>
2025-07-17 07:32:57 +0530

    Updated migration files

--- azura_be/users/models.py
+++ azura_be/users/models.py
@@ -7,6 +7,7 @@ from azura_be.base.constants import DayChoices
 from timezone_field.fields import TimeZoneField
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
+from django.contrib.postgres.fields import ArrayField
 
 from azura_be.base.models import BaseModel
 from azura_be.core.models import Specialty
@@ -19,14 +20,14 @@ class User(AbstractUser):
     last_name = models.CharField(_("Last Name of User"), blank=True, max_length=255)
     email = models.EmailField(_("email address"), unique=True)
     picture = models.ImageField(null=True, blank=True)
-    timezone = TimeZoneField()
+    timezone = TimeZoneField(null=True, blank=True)
     email_noftification = models.BooleanField(default=True)
     sms_notification = models.BooleanField(default=True)
     auto_form_save = models.BooleanField(default=False)
 
-    provider_groups = models.ManyToManyField("provider_groups.ProviderGroup", blank=True)
-    departments = models.ManyToManyField("provider_groups.Department", blank=True)
-    primary_location = models.ForeignKey("locations.Location", on_delete=models.SET_NULL, null=True, blank=True)
+    provider_groups = ArrayField(models.UUIDField(), null=True, blank=True)
+    departments = ArrayField(models.UUIDField(), null=True, blank=True)
+    primary_location = models.UUIDField(null=True, blank=True)
     phone = models.CharField(
         max_length=15,
         validators=[
@@ -38,7 +39,7 @@ class User(AbstractUser):
         ],
         blank=True,
     )
-    role = models.CharField(max_length=255)
+    role = models.CharField(max_length=255, blank=True)
 
     account_user = models.BooleanField(default=False)
     two_factor_auth = models.BooleanField(default=False)
@@ -80,6 +81,6 @@ class License(BaseModel):
 
 class WorkShedule(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="schedules")
-    day = models.CharField(max_length=5, choices=DayChoices)
+    day = models.CharField(max_length=10, choices=DayChoices)
     start = models.TimeField()
     end = models.TimeField()
